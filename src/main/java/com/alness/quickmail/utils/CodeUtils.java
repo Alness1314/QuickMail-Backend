e338399a5c7424b49f18b7c42c839a1fbcf68c07
Alberto Ordaz <jesus.ordaz@sng.biz>
2024-09-05 10:16:51 -0600

    feat: correcciones para godaddy mail

--- src/main/java/com/alness/quickmail/utils/CodeUtils.java
+++ src/main/java/com/alness/quickmail/utils/CodeUtils.java
@@ -4,7 +4,7 @@ public class CodeUtils {
     public static final String API_CODE_400 = "QM-400";
     public static final String API_CODE_404 = "QM-404";
     public static final String API_CODE_409 = "QM-409";
-    public static final String API_CODE_500 = "QM-400";
+    public static final String API_CODE_500 = "QM-500";
 
     private CodeUtils() {
         throw new IllegalStateException("Utility class");
